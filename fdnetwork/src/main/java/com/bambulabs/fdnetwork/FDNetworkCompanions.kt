c7b7aee0d6c2118b76b99e6352f6bcc6991309b6
Serhat Erfidan <serhat@finedinemenu.com>
2021-04-07 13:03:11 +0300

    Staging update

--- fdnetwork/src/main/java/com/bambulabs/fdnetwork/FDNetworkCompanions.kt
+++ fdnetwork/src/main/java/com/bambulabs/fdnetwork/FDNetworkCompanions.kt
@@ -4,20 +4,31 @@ class FDNetworkCompanions {
     companion object {
 
         var authenticationToken: String? = null
+        var isStagingMode: Boolean = false
 
-        var baseUrl = "https://api.finedinemenu.com/v1/"
-        var panelUrl = "https://panel.finedinemenu.com/"
+        var baseUrl = "https://api-staging.finedinemenu.com/"
+        var panelUrl = "http://fdmetronic.herokuapp.com/"
 
         fun setStagingActive(boolean: Boolean){
             if (boolean) {
 
-                baseUrl = "https://finedine-api-staging.herokuapp.com/v1"
+                baseUrl = "https://api-staging.finedinemenu.com/"
                 panelUrl = "http://fdmetronic.herokuapp.com/"
+
+                // baseUrl = "http://192.168.1.52:8080/"
+                // panelUrl = "http://192.168.1.52:5000/"
+
+                isStagingMode = true
 
             } else {
 
-                baseUrl = "https://api.finedinemenu.com/v1/"
+                baseUrl = "https://api.finedinemenu.com/"
                 panelUrl = "https://panel.finedinemenu.com/"
+
+                // baseUrl = "http://192.168.1.52:8080/"
+                // panelUrl = "http://192.168.1.52:5000/"
+
+                isStagingMode = false
 
             }
 
